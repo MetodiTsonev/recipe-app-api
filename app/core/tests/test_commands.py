647264a3cb8edaa53abe237cf98e6f7edc05e09f
Metodi Tsonev <metodi.tsonev@gmail.com>
2023-12-05 11:15:32 +0200

    Fixed Tests Porblems

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,7 +6,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.managment.commands.wait_for_db.Command.check')
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
 
 
@@ -16,7 +16,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(databases=('default'))
+        patched_check.assert_called_once_with(databases=(['default']))
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
