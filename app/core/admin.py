b7d038045286922eefcd61b904b55fcc40c55117
Metodi Tsonev <metodi.tsonev@gmail.com>
2023-12-05 11:56:13 +0200

    Cofigured docker-compose and checks to wait for db

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,3 @@
-# TODO
-# from django.contrib import admin
+from django.contrib import admin # noqa
 
 # Register your models here.
--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,3 @@
-# TODO
-# from django.db import models
+from django.db import models # noqa
 
 # Create your models here.
