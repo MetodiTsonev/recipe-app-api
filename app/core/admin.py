7eef4adeacca3621c361f14ef20b74ad4a557020
Metodi Tsonev <metodi.tsonev@gmail.com>
2023-12-22 11:46:16 +0200

    Added Admin Customizations and Tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,48 @@
-from django.contrib import admin # noqa
+"""
+Django Admin Customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+#Future Proofing
+from django.utils.translation import gettext_lazy as _
+
+class UserAdmin(BaseUserAdmin):
+    """Define th admin pages for users"""
+    ordering = ['id']
+    list_display = ('email', 'name')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'),{'fields': ('last_login',)}),
+    )
+    # add the fields that we want to be able to edit from the admin page
+    readonly_fields = ('last_login',)
+    # Now register this new user admin class with our User model.
+    # This tells Django that we want to use our custom UserAdmin class instead of the built-in one.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                )
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
