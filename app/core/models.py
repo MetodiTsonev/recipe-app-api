4767494a2d47fa6c358462a9b61c71103b591a1c
Metodi Tsonev <metodi.tsonev@gmail.com>
2023-12-22 10:52:27 +0200

    code additions

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    '''Manager for users.'''
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email, and password."""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
